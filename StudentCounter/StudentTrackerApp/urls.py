bb14674f25a7b686f889684ad4fa09398eb16a94
Ata Reenes <lifeonshort@gmail.com>
2022-08-28 16:01:40 +0300

    Not resolved file removed

--- StudentCounter/StudentTrackerApp/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from StudentTrackerApp import views
-
-urlpatterns = [
-    path('main', views.index, name='main'),
-    path('main', views.add_student , name='add'),
-]
