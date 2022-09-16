530286cac5bf8555ea4fd2148f5e6a9598d24b9a
Ata Reenes <lifeonshort@gmail.com>
2022-09-16 23:16:20 +0300

    Bug fixed v2.1

--- YouTubeCv/backend/cfe/settings.py
+++ YouTubeCv/backend/cfe/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import environ
+
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-tsaqni4_di3o%j+cldqrg0d94gx=sbv&(z2gv^qbv)y0u9sodt'
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
